5f25771f058c816405627eda7ca8c73eda4108e5
kashif <balochkashif1444@gmail.com>
2025-05-03 19:46:45 +0500

    made responsive

--- script.js
+++ script.js
@@ -23,19 +23,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   
-  $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      loop: true,
-      margin: 20,
-      nav: true,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: 2000,
-      responsive: {
-        0: { items: 1 },
-        600: { items: 2 },
-        1000: { items: 3 }
-      }
-    });
-  });
-
+  
